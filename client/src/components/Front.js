a29b558defdfcd532a224c95442922e87a176640
Shyam-12 <72685424+Shyam-12@users.noreply.github.com>
2022-09-11 14:43:15 +0530

    Updated Changes

--- client/src/components/Front.js
+++ client/src/components/Front.js
@@ -1,18 +1,22 @@
-
+import img from '../Images/woman.png'
 
 const Front = () => {
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10 mt-20">
+                <div className="flex flex-1 justify-start flex-col md:mr-10 mt-40">
                     <h1 className="text-xl sm:text-3xl text-pink-400  py-1">lorem ipsum dolor emet</h1>
                     <p className="text-left mt-5 text-pink-400 font-liight md:w-0/12 w-11/12 text-base">lorem ipsum dolor emet lorem ipsum lorem</p>
-                    <button type="button"className="flex flex-row justify-center items-center my-5 bg-blue-700 p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" >
-                        Send Text
+                    <button type="button"className="w-48 flex flex-row justify-center items-center my-5 bg-blue-700 p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" >
+                        <p className="text-white text-base font-semibold">Send Text</p>
                     </button>
-                    <button type="button"className="flex flex-row justify-center items-center my-5 bg-blue-700 p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" >
-                        Call 100
+                    <button type="button"className="w-48 flex flex-row justify-center items-center my-5 bg-blue-700 p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" >
+                        <p className="text-white text-base font-semibold">Call 100</p>
                     </button>
+                </div>
+
+                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+                     <img src={img} />
                 </div>
             </div>
         </div>
