05674070931f767a60bcc41beb860ca4470cc4c5
tanishk-agarwal <88491381+tanishk-agarwal@users.noreply.github.com>
2022-09-11 16:37:26 +0530

    added files

---
+++ client/src/components/Awareness.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Awareness = () => {
+  return (
+    <div>
+      <p>
+        <a href="https://www.smilefoundationindia.org/p_awareness_and_safety_campaign.html">
+          Article 1
+        </a>
+      </p>
+      <p>
+        <a href="https://www.unilever.com/planet-and-society/respect-human-rights/promoting-safety-for-women/">
+          Article 2
+        </a>
+      </p>
+      <p>
+        <a href="https://www.thesafetybox.org/safety-awareness-tips-for-women-teens-and-girls">
+          Article 3
+        </a>
+      </p>
+      <p>
+        <a href="https://www.unwomen.org/en/what-we-do/ending-violence-against-women/creating-safe-public-spaces">
+          Article 4
+        </a>
+      </p>
+      <p>
+        <a href="https://issuesiface.com/magazine/top-10-safety-tips-for-women">
+          Article 5
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Awareness;
---
+++ client/src/components/laws.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const laws = () => {
+  return (
+    <div>
+      <p>
+        <a href="http://ncw.nic.in/important-links/List-of-Laws-Related-to-Women">
+          Laws
+        </a>
+      </p>
+      <p>
+        <a href="https://yourstory.com/2016/06/laws-that-protect-women-rights/amp">
+          Laws
+        </a>
+      </p>
+      <p>
+        <a href="https://www.amnesty.org/en/what-we-do/discrimination/womens-rights/">
+          Laws
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default laws;
